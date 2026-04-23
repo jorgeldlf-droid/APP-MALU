c53e348580cb551cc0f3157295bc208de2c31abe
jorgeldlf-droid <jorgeldlf@hotmail.com>
2026-04-23 16:04:34 -0300

    Change export syntax from ES6 to CommonJS

--- api/health.js
+++ api/health.js
@@ -1,6 +1,6 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.status(200).json({
     ok: true,
     transcriptionConfigured: Boolean(process.env.OPENAI_API_KEY)
   });
-}
+};
